e39146c6b376c11790fa67fb29093b3172234832
Brian Mancini <bmancini@gmail.com>
2017-02-11 19:37:39 -0500

    Updated service manager to scale service

--- swarm/service-manager/src/docker-client.js
+++ swarm/service-manager/src/docker-client.js
@@ -1,11 +1,8 @@
 import docker from 'docker-remote-api';
 import isDocker from 'is-docker';
-import fs from 'fs';
 
-// this is the name of the `docker deploy`. all services will be prefixed
-// with this prefix name to allow duplicate deployments on the same cluster.
-// this could be provided by environment variable instead of hard coded
-let swarmPrefix = 'swarm_';
+let API_VERSION = 'v1.26';
+let STACK_NAME = process.env.STACK_NAME || 'test';
 
 // detects if running inside a docker container and mounts to volume
 // provided at run time, otherwise it will use the default docker
@@ -14,11 +11,11 @@ let swarmPrefix = 'swarm_';
 let host = isDocker() ? '/tmp/docker.sock' : '/var/run/docker.sock';
 
 // create a remote api client
-let request = docker({ host });
+let request = docker({ host,  });
 
 export default {
   getService,
-  createService,
+  updateReplicas,
 };
 
 /**
@@ -26,43 +23,106 @@ export default {
  * @param  {[type]} name [description]
  * @return {[type]}      [description]
  */
-function getService(name) {
+async function getService(name) {
+  console.log(`fetching service ${STACK_NAME}_${name}`);
+  return await get(`/services/${STACK_NAME}_${name}`);
+}
+
+
+/**
+ * This process loads the current service definition, reads the spec, and
+ * then performs and update on the Replicas.  This functionality is similar to
+ * Docker CLI
+ * https://github.com/docker/docker/blob/1.13.x/cli/command/service/scale.go#L85
+ * @param  {[type]} name    [description]
+ * @param  {[type]} updates [description]
+ * @return {[type]}         [description]
+ */
+async function updateReplicas(name, replicas) {
+  console.log(`updating service ${STACK_NAME}_${name} to ${replicas} replicas`);
+  let service = await getService(name);
+
+  let spec = service.Spec;
+  spec.Mode.Replicated.Replicas = replicas;
+
+  let version = service.Version.Index;
+  let url = `/services/${STACK_NAME}_${name}/update?version=${version}`;
+  return await post(url, spec);
+}
+
+
+
+////////////////////////////////////////
+///
+
+
+function get(url) {
   return new Promise((resolve, reject) => {
-    request.get(`/services/${swarmPrefix}${name}`, { json: true }, (err, result) => {
+    request.get(url, { json: true, version: API_VERSION  }, (err, result) => {
       if(err) reject(err);
       else    resolve(result);
     });
   });
 }
 
-/**
- * [createService description]
- * @param  {[type]} name    [description]
- * @param  {Number} desired [description]
- * @return {[type]}         [description]
- */
-function createService(name, desired = 1) {
+function post(url, data) {
   return new Promise((resolve, reject) => {
-    let req = request.post(`/services/${name}`, { json: true }, (err, result) => {
-
-    });
-  });
-}
-
-/**
- * [loadConfig description]
- * @param  {[type]} name [description]
- * @return {[type]}      [description]
- */
-function loadConfig(name) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(`configs/${name}.json`, (err, buffer) => {
+    request.post(url, { json: data, version: API_VERSION }, (err, result) => {
       if(err) reject(err);
-      else {
-        let text = buffer.toString();
-        let json = JSON.parse(text);
-        resolve(json);
-      }
+      else    resolve(result);
     });
   });
 }
+
+
+// SAMPLE SERVICE
+/*
+{
+  "ID": "tkzqrrsy877g0hxjy9cyjrrc1",
+  "Version": {
+    "Index": 1898
+  },
+  "CreatedAt": "2017-02-11T23:32:23.020330768Z",
+  "UpdatedAt": "2017-02-11T23:32:23.020330768Z",
+  "Spec": {
+    "Name": "test_elasticsearch5",
+    "TaskTemplate": {
+      "ContainerSpec": {
+        "Image": "elasticsearch:5@sha256:8f4c23871494b6baacbaf0c9f6ee48db1b5c62de33f9c4c14b2e17f1922e56ee",
+        "DNSConfig": {}
+      },
+      "Resources": {
+        "Limits": {},
+        "Reservations": {}
+      },
+      "RestartPolicy": {
+        "Condition": "any",
+        "MaxAttempts": 0
+      },
+      "Placement": {},
+      "ForceUpdate": 0
+    },
+    "Mode": {
+      "Replicated": {
+        "Replicas": 1
+      }
+    },
+    "UpdateConfig": {
+      "Parallelism": 1,
+      "FailureAction": "pause",
+      "MaxFailureRatio": 0
+    },
+    "EndpointSpec": {
+      "Mode": "vip"
+    }
+  },
+  "Endpoint": {
+    "Spec": {}
+  },
+  "UpdateStatus": {
+    "StartedAt": "0001-01-01T00:00:00Z",
+    "CompletedAt": "0001-01-01T00:00:00Z"
+  }
+}
+
+ */
